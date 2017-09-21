d909193f295f79a30888847daa8c30f80aaf76b4
PrintG <2662256509@qq.com>
2017-09-21 19:11:25 +0800

    增加兼容性前缀

--- js/index.js
+++ js/index.js
@@ -3,7 +3,6 @@
 //     * QQ -> 2662256509       //
 /*=============================*/
 ~function(){
-<<<<<<< HEAD
 	//swiper
 	var mainSwiper = new Swiper("#main-swiper", {
 		autoplay : 20000,
@@ -16,17 +15,3 @@
 		paginationType : "progress"
 	});
 }();
-=======
-    //swiper
-    var mainSwiper = new Swiper("#main-swiper", {
-    	    autoplay : 20000,
-	    autoplayDisableOnInteraction : false,
-	    //button
-	    prevButton : ".swiper-button-prev",
-	    nextButton : ".swiper-button-next",
-	    //progress
-	    pagination : ".swiper-pagination",
-	    paginationType : "progress"
-    });
-}();
->>>>>>> 20951c321eed18ad0ba69c046210a8e923f487b2
