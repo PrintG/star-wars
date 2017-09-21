f0a4e05c241898682e7d05e2600925ec6e9c0209
PrintG <2662256509@qq.com>
2017-09-21 10:56:22 +0800

    demo_v1.0

---
+++ js/index.js
@@ -0,0 +1,23 @@
+/*==============================*/
+//     * author -> Print        //
+//     * QQ -> 2662256509       //
+/*=============================*/
+require.config({
+    paths : {
+    	"swiper" : "./lib/swiper-3.4.2.min"
+    }
+});
+require(["swiper"], function(swiper){
+	//swiper
+	var mainSwiper = new Swiper("#main-swiper", {
+		autoplay : 5000,
+		autoplayDisableOnInteraction : false,
+		//button
+		prevButton : ".swiper-button-prev",
+		nextButton : ".swiper-button-next",
+		//progress
+		pagination : ".swiper-pagination",
+		paginationType : "progress"
+	});
+	
+});
